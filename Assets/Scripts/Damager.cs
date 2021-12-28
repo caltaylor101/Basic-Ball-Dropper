3b5f92418c5baebd8696a31781060c1c0d55124c
cognizantllamas <62909943+cognizantllamas@users.noreply.github.com>
2021-12-28 16:38:04 -0700

    Ball Explosions
    Added ball explosions of confetti.

--- Assets/Scripts/Damager.cs
+++ Assets/Scripts/Damager.cs
@@ -7,8 +7,52 @@ public class Damager : MonoBehaviour
    
     // Start is called before the first frame update
     private int ballCount;
+    private SpriteRenderer render;
+    private GameObject ballSpawnPoints;
+    private Transform ballSpawnLocation;
+    private List<Spawner> spawnPoints;
+    private Spawner spawn1;
+    private Spawner spawn2;
+    private Spawner spawn3;
+    private Spawner spawn4;
+    private Spawner spawn5;
+
+
     void Start()
     {
+        render = GetComponent<SpriteRenderer>();
+        ballSpawnPoints = GameObject.Find("BallSpawnPoints");
+        spawn1 = GameObject.Find("BallSpawnPoints/Spawner_1").GetComponent<Spawner>();
+        spawn2 = GameObject.Find("BallSpawnPoints/Spawner_2").GetComponent<Spawner>();
+        spawn3 = GameObject.Find("BallSpawnPoints/Spawner_3").GetComponent<Spawner>();
+        spawn4 = GameObject.Find("BallSpawnPoints/Spawner_4").GetComponent<Spawner>();
+        spawn5 = GameObject.Find("BallSpawnPoints/Spawner_5").GetComponent<Spawner>();
+
+        spawnPoints = new List<Spawner>();
+        spawnPoints.Add(spawn1);
+        spawnPoints.Add(spawn2);
+        spawnPoints.Add(spawn3);
+        spawnPoints.Add(spawn4);
+        spawnPoints.Add(spawn5);
+        spawnPoints.Add(spawn1);
+        spawnPoints.Add(spawn2);
+        spawnPoints.Add(spawn3);
+        spawnPoints.Add(spawn4);
+        spawnPoints.Add(spawn5);
+        spawnPoints.Add(spawn1);
+        spawnPoints.Add(spawn2);
+        spawnPoints.Add(spawn3);
+        spawnPoints.Add(spawn4);
+        spawnPoints.Add(spawn5);
+        spawnPoints.Add(spawn1);
+        spawnPoints.Add(spawn2);
+        spawnPoints.Add(spawn3);
+        spawnPoints.Add(spawn4);
+        spawnPoints.Add(spawn5);
+
+
+
+
     }
 
     // Update is called once per frame
@@ -27,11 +71,25 @@ public class Damager : MonoBehaviour
             otherScript.score = otherScript.score + (otherScript.scoreMultiplier * otherScript.scoreValue);
             DestroyBall();
         }
-
     }
 
     private void DestroyBall()
     {
+       // Debug.Log(ballSpawnPoints.GetComponent<Transform>().position);
+        //ballSpawnLocation = ballSpawnPoints.GetComponent<Transform>();
+        //ballSpawnLocation = gameObject.transform;
+        //Debug.Log(ballSpawnLocation.position);
+        //Debug.Log(ballSpawnPoints.GetComponent<Transform>().position);
+        //Debug.Log("difference");
+        //Debug.Log(gameObject.transform.position);
+        render.enabled = false;
+        ballSpawnPoints.GetComponent<Transform>().position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 1);
+
+        foreach (Spawner spawn in spawnPoints)
+        {
+            spawn.Spawn();
+        }
         Destroy(gameObject);
+        Debug.Log(ballSpawnPoints.GetComponent<Transform>().position);
     }
 }
