67d8bf08442d4cd56d6f3580400775ebca207d32
cognizantllamas <62909943+cognizantllamas@users.noreply.github.com>
2022-01-04 13:36:49 -0700

    Upgrade Hourglass Obstacle
    Created a button to upgrade the hourglass obstacle.

--- Assets/Scripts/UpgradeObstacles.cs
+++ Assets/Scripts/UpgradeObstacles.cs
@@ -9,6 +9,10 @@ public class UpgradeObstacles : MonoBehaviour
     public GameObject gameRun;
     public int upgradeObstacleCost = 10;
     public int upgradeObstacleCost2 = 100;
+
+    public GameObject hourglassImage;
+    public GameObject hourglassCollider;
+    public int upgradeObstacleCost3 = 1000;
     public void UpgradeObstacle1()
     {
         obstacles = GameObject.FindGameObjectsWithTag("Obstacle1");
@@ -36,6 +40,18 @@ public class UpgradeObstacles : MonoBehaviour
             {
                 obstacle.GetComponent<Transform>().localScale = obstacle.GetComponent<Transform>().localScale - new Vector3(0.02f, 0.02f, 0.02f);
             }
+        }
+    }
+
+    public void UpgradeObstacle3()
+    {
+        if (gameRun.GetComponent<ImageFade>().score >= upgradeObstacleCost3)
+        {
+            gameRun.GetComponent<ImageFade>().score = gameRun.GetComponent<ImageFade>().score - upgradeObstacleCost3;
+            upgradeObstacleCost3 = (int)System.Math.Ceiling((float)upgradeObstacleCost3 * 1.75f);
+            hourglassImage.GetComponent<Transform>().localScale += new Vector3(.02f, .0f, .0f);
+            hourglassCollider.GetComponent<Transform>().localScale += new Vector3(.02f, .0f, .0f);
+            
         }
     }
 }
