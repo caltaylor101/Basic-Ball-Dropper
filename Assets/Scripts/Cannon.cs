bb95c738c9e641791c1a0e447accabb0ce97119b
cognizantllamas <62909943+cognizantllamas@users.noreply.github.com>
2022-01-10 14:23:47 -0700

    Downward Shooting
    Cannon can now shoot downward

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -41,13 +41,17 @@ public class Cannon : MonoBehaviour
         if (cannonBullets == 0)
         {
             cannonAnim.SetBool("shootCannonball", false);
+            Debug.Log(false);
+
         }
+
     }
 
     public void ShootCannonballRight()
     {
         cannonBullets -= 1;
-        GameObject newCannonball = Instantiate(cannonball, new Vector3(6.99f, 2918.04f, 1), Quaternion.identity);
+        //cannonAnim.SetInteger("cannonBullets", cannonBullets);
+        GameObject newCannonball = Instantiate(cannonball, new Vector3(7.5f, 2918.233f, 1), Quaternion.identity);
         //newCannonball.GetComponent<Rigidbody2D>().velocity = transform.right * 10f;
         //newCannonball.tag = "cannonTest";
         newCannonball.GetComponent<Rigidbody2D>().AddForce((Vector2.right * 600f) + (Vector2.down * 600f));
@@ -58,8 +62,18 @@ public class Cannon : MonoBehaviour
     public void ShootCannonballLeft()
     {
         cannonBullets -= 1;
+        //cannonAnim.SetInteger("cannonBullets", cannonBullets);
         GameObject newCannonball = Instantiate(cannonball, new Vector3(1.449667f, 2918.233f, 1), Quaternion.identity);
         newCannonball.GetComponent<Rigidbody2D>().AddForce(-(Vector2.right * 600f) + (Vector2.down * 600f));
 
+    }
+
+    public void ShootCannonballDown()
+    {
+        cannonBullets -= 1;
+        Debug.Log(cannonBullets);
+        //cannonAnim.SetInteger("cannonBullets", cannonBullets);
+        GameObject newCannonball = Instantiate(cannonball, new Vector3(5.054342f, 2917.4f, 1), Quaternion.identity);
+        newCannonball.GetComponent<Rigidbody2D>().AddForce((Vector2.down * 850f));
     }
 }
