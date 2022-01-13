7431065a8b3778c6728c98cbc4c67615db754dea
cognizantllamas <62909943+cognizantllamas@users.noreply.github.com>
2022-01-13 09:02:11 -0700

    Other design
    Other design start.

---
+++ Assets/Gear.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+            var hinge = GetComponent<HingeJoint2D>();
+            // Make the hinge motor rotate with 90 degrees per second and a strong force.
+            var motor = hinge.motor;
+            hinge.motor = motor;
+            hinge.useMotor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
